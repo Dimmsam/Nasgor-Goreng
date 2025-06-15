82278d4cc171349500d9aabb89b8246d0e9e5912
MahesaFazrie <mahesa.fazrie06@gmail.com>
2025-06-15 13:53:54 +0700

    mengganti nama folder body menjadi scr dan memperbaharui stack

--- header/stack.h
+++ header/stack.h
@@ -1,30 +1,49 @@
 #ifndef STACK_H
 #define STACK_H
 
-typedef struct RiwayatNode { 
-    char judul[100]; 
-    struct RiwayatNode* next; 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+// Struct buat node Stack (riwayat bacaan)
+typedef struct RiwayatNode {
+    char judul[100];
+    char genre[50];
+    struct RiwayatNode* next;
 } RiwayatNode;
 
+// Struct buat Stack
+typedef struct Stack {
+    RiwayatNode* top;
+} Stack;
 
-void tambahRiwayat(RiwayatNode** top, const char* judul);
-// IS : top menunjuk ke node teratas stack atau NULL jika kosong
-// FS :  menunjuk ke node baru yang berisi judul, dengan next menunjuk ke node sebelumnya.
+// Struct buat User
+typedef struct User {
+    char userName[50];
+    Stack* riwayatBaca;
+    struct User* next;
+} User;
 
-void hapusRiwayatTerakhir(RiwayatNode** top);
-// IS : top menunjuk ke node teratas stack jika tidak NULL
-// FS : top menunjuk ke node sebelumnya, dan memory node teratas dibebaskan.
+// Stack Operations
+Stack* createStack();
+void push(Stack* stack, const char* judul, const char* genre);
+RiwayatNode* pop(Stack* stack);
+bool isStackEmpty(Stack* stack);
 
-void lihatBukuTerakhir(RiwayatNode* top);
-// IS : top menunjuk ke node teratas stack jika tidak NULL
-// FS : menampilkan judul buku terakhir yang ditambahkan ke stack.
+// Display Functions
+void displayReadingHistory(Stack* stack);
+void displayUserHistory(const char* userName);
 
-void tampilkanRiwayat(RiwayatNode* top);
-// IS : top menunjuk ke node teratas stack jika tidak NULL
-// FS : menampilkan judul buku yang ditambahkan dari atas kebawah.
+// File Operations
+void saveHistoryToFile(Stack* stack, const char* userName);
+void loadHistoryFromFile(Stack** stack, const char* userName);
 
-void resetRiwayat(RiwayatNode** top);
-// IS : top menunjuk ke node teratas stack jika tidak NULL
-// FS : top menunjuk ke NULL, dan semua node di stack dibebaskan.
+// User Management
+Stack* getUserStack(const char* userName);
+void addBookToUserHistory(const char* userName, const char* judul, const char* genre);
 
-#endif
+// Global variable
+extern User* userList;
+
+#endif // STACK_H
